7a2b62f91fdcd39bb9328e23b578fd8beebc9740
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2024-01-29 13:23:05 +0100

    added functionality of hasLowercaseLetters() added functionality of hasUppercaseLetters()

--- src/main/java/neuefische/de/Main.java
+++ src/main/java/neuefische/de/Main.java
@@ -2,22 +2,34 @@ package neuefische.de;
 
 public class Main {
     public static void main(String[] args) {
+        hasLowercaseLetters("asdflkjasdf");
         sayHelloWorld();
     }
 
     public static void sayHelloWorld() {
-        System.out.println("Hello world!");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello world!\n");
+        sb.append("in this recap lecture we will ignore the fact that REGEX exists!\n");
+        System.out.println(sb.toString());
     }
 
     public static Boolean hasAtLeast8Characters(String pw) {
-        return false;
+        return pw.length() > 7;
     }
 
     public static Boolean hasLowercaseLetters(String pw) {
+        char[] allowedCharacters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        for(char c : allowedCharacters){
+            if (pw.contains("" + c)) return true;
+        }
         return false;
     }
 
     public static Boolean hasUppercaseLetters(String pw) {
+        char[] allowedCharacters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().toCharArray();
+        for(char c : allowedCharacters){
+            if (pw.contains("" + c)) return true;
+        }
         return false;
     }
 
--- src/test/java/neuefische/de/MainTest.java
+++ src/test/java/neuefische/de/MainTest.java
@@ -71,5 +71,4 @@ class MainTest {
         // THEN
         assertTrue(actual);
     }
-
 }
